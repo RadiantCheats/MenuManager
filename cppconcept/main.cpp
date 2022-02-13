994dd9f45be28b7ccaa9a34d1a564bd88d7b3dd5
Luck <luckplaysmcpe@gmail.com>
2022-02-12 19:35:19 -0500

    C++ Concept

---
+++ cppconcept/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#pragma comment(lib, "urlmon.lib")
+
+int main()
+{
+    std::cout << "a" << "\n";
+    /*
+    std::string url = "https://cherax.vip/loader.php?download=true";
+    std::string path = "C:\\tmp\\CheraxLoader.zip";
+    URLDownloadToFile(NULL, url.c_str(), path.c_str(), 0, NULL);
+    */
+    return 0;
+}
